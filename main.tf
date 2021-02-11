710d1308d0590d6e70520ee8c4055d39ed9cd448
nikcbg <nikcbg@yahoo.com>
2021-02-11 10:33:10 +0200

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,19 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "region" {}
+variable "ami" {}
+variable "instance_type" {}
+# ami-085925f297f89fce1
+
+provider "aws" {
+  access_key = var.access_key
+  secret_key = var.secret_key
+  region = var.region
+}
+resource "aws_instance" "web123" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  tags = {
+    Name = "my-test-instance"
+  }
+}
